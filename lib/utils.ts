5dc7efa6833b9fb8a9e9f71ade54807ad3e01b51
Leonardo Decanini <leodede04@gmail.com>
2025-08-06 16:00:52 -0300

    asd

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,20 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-
 export const ServerUrl = true
-  ? 'https://api.woo.moveup.digital'
-  /* : 'https://79j3r6m3-3000.brs.devtunnels.ms'; */
-  : 'http://localhost:3000';
+  ? "https://api.woo.moveup.digital"
+  : /* : 'https://79j3r6m3-3000.brs.devtunnels.ms'; */
+    "http://localhost:3000";
 
 export const getImageUrl = (file: any): string => {
-  const datePath = file.createdAt.slice(0, 10).replace(/-/g, '/');
-  const relativePath = `desarrurbana/${datePath}/${file.filename}`;
-
-  console.log(`https://cdn.moveup.digital/${relativePath}`);
-  return `https://cdn.moveup.digital/${relativePath}`;
+  return `https://cdn.moveup.digital/${file.path}`;
 };
